25f963e771a22a7013af4cbc6cf32713a15ee673
grantrudow <58617204+wrangler13@users.noreply.github.com>
2020-02-06 07:43:05 -0500

    Initial Commit

---
+++ ToDo_MasteringSUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDo_MasteringSUI
+//
+//  Created by Grant Rudow on 2/6/20.
+//  Copyright © 2020 Grant Rudow. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
